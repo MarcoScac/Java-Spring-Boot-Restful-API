ccc404fc6be33d37a7f6a14724ec8a7136f38420
MarcoScac <80743269+MarcoScac@users.noreply.github.com>
2021-03-24 11:39:37 +0100

    Update

---
+++ muster/muster/src/main/java/com/example/muster/MusterApplication.java
@@ -0,0 +1,13 @@
+package com.example.muster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusterApplication.class, args);
+	}
+
+}
---
+++ muster/muster/src/test/java/com/example/muster/MusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
